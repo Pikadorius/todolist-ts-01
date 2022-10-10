7e7b903238e2011bb8520335707b08db0bf8be1b
Pikadorius <karpukea@yandex.by>
2022-10-10 21:41:11 +0300

    added BLL consts to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import './App.css';
 import TodoList from "./TodoList";
 
+export type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
 function App() {
+    //BLL
+    const todoListTitle : string = "What to learn";
+    const tasksForTodoList : Array<TaskType> = [
+        {id: 1, title: "HTML & CSS", isDone: true},
+        {id: 2, title: "JS & ES6", isDone: true},
+        {id: 3, title: "REACT & TS", isDone: false},
+    ];
+
+    //GUI
     return (
         <div className="App">
-            <TodoList title={"What to learn"}/>
-            <TodoList title={"What to buy"}/>
+            <TodoList title={todoListTitle} tasks={tasksForTodoList}/>
         </div>
     );
 }
