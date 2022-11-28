e8ef642f69fb0f92bcef2e06dbb38286e55e1b82
Pikadorius <karpukea@yandex.by>
2022-11-28 14:12:41 +0300

    added some features for EditableSpan.tsx (error, send onEnter)

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,5 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
-
+import React, {ChangeEvent, useState, KeyboardEvent} from 'react';
 
 type EditableSpanPropsType = {
     value: string
@@ -9,20 +8,37 @@ type EditableSpanPropsType = {
 export function EditableSpan(props: EditableSpanPropsType) {
     let [editMode, setEditMode] = useState(false);
     let [title, setTitle] = useState(props.value);
+    const [error, setError] = useState(false)
 
     const activateEditMode = () => {
         setEditMode(true);
-        setTitle(props.value);
+        // setTitle(props.value);
     }
     const activateViewMode = () => {
-        setEditMode(false);
-        props.onChange(title);
+        if (title.trim()) {
+            setEditMode(false);
+            props.onChange(title);
+        } else {
+            setError(true)
+        }
     }
+
+    const onEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key==='Enter') {
+            activateViewMode()
+        }
+    }
+
     const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
 
     return editMode
-        ? <input value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}/>
+        ? <input placeholder={error ? 'Error! Write something!' : 'Enter your text...'}
+                 value={title}
+                 onChange={changeTitle}
+                 autoFocus
+                 onBlur={activateViewMode}
+                 onKeyDown={onEnterHandler}/>
         : <span onDoubleClick={activateEditMode}>{props.value}</span>
 }
